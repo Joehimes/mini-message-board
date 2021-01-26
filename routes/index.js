131ea5baa9e3ef83e0ea9a8705d488c4525b11ec
Joe Himes <joehimes42@gmail.com>
2021-01-26 16:25:14 -0500

    Ready for deploy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Joe',
+    added: new Date()
+  },
+  {
+    text: "What up?",
+    user: 'Bob',
+    added: new Date()
+  }
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
+
+/* GET new message page */
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+/* POST new message */
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.text, user: req.body.name, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
